fbf679b53951f44c17318c1ba7fbaa3774c37531
Daisuke Morita <daisuke.marubinotto@gmail.com>
2016-11-28 21:05:57 +0900

    Disable Ecto for now

--- lib/cotoami.ex
+++ lib/cotoami.ex
@@ -9,7 +9,7 @@ defmodule Cotoami do
     # Define workers and child supervisors to be supervised
     children = [
       # Start the Ecto repository
-      supervisor(Cotoami.Repo, []),
+      # supervisor(Cotoami.Repo, []),
       # Start the endpoint when the application starts
       supervisor(Cotoami.Endpoint, []),
       # Start your own worker by calling: Cotoami.Worker.start_link(arg1, arg2, arg3)
