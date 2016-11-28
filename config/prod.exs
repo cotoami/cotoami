51e19a6c0b8c4b06ed8b5b62848c9498d6f8996e
Daisuke Morita <daisuke.marubinotto@gmail.com>
2016-11-28 23:17:59 +0900

    Delete the reference to prod.secret.exs in prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -58,4 +58,4 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
