d24a25dbe74ef18af1b231cd6b24854b81d9b87c
Daisuke Morita <daisuke.marubinotto@gmail.com>
2016-12-07 20:23:22 +0900

    Format elm code

--- web/elm/App.elm
+++ web/elm/App.elm
@@ -12,154 +12,176 @@ import String
 import Markdown
 import Keys exposing (ctrl, meta, enter)
 
+
 main : Program Never Model Msg
-main = 
-  Html.program { 
-    init = init, 
-    view = view, 
-    update = update, 
-    subscriptions = subscriptions
-  }
+main =
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
 
 
 -- MODEL
 
-type alias Model = {
-  ctrlDown : Bool,
-  editingNewPost : Bool,
-  newPost: String,
-  posts : List String
-}
+
+type alias Model =
+    { ctrlDown : Bool
+    , editingNewPost : Bool
+    , newPost : String
+    , posts : List String
+    }
+
 
 initModel : Model
-initModel = 
-  {
-    ctrlDown = False,
-    editingNewPost = False,
-    newPost = "",
-    posts = []
-  }
+initModel =
+    { ctrlDown = False
+    , editingNewPost = False
+    , newPost = ""
+    , posts = []
+    }
 
-init : (Model, Cmd Msg)
-init = 
-  (initModel, Cmd.none)
+
+init : ( Model, Cmd Msg )
+init =
+    ( initModel, Cmd.none )
+
 
 
 -- UPDATE
 
-type Msg 
-  = NoOp
-  | KeyDown KeyCode
-  | KeyUp KeyCode
-  | FocusNewPostEditor
-  | BlurNewPostEditor
-  | InputNewPost String
-  | Post
-  | KeyDownInNewPostEditor KeyCode
-  
-update : Msg -> Model -> (Model, Cmd Msg)
+
+type Msg
+    = NoOp
+    | KeyDown KeyCode
+    | KeyUp KeyCode
+    | FocusNewPostEditor
+    | BlurNewPostEditor
+    | InputNewPost String
+    | Post
+    | KeyDownInNewPostEditor KeyCode
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    NoOp ->
-      model ! []
+    case msg of
+        NoOp ->
+            model ! []
 
-    KeyDown key -> 
-      if key == ctrl.keyCode || key == meta.keyCode then
-        ({ model | ctrlDown = True }, Cmd.none)
-      else
-        (model, Cmd.none)
-        
-    KeyUp key -> 
-      if key == ctrl.keyCode || key == meta.keyCode then
-        ({ model | ctrlDown = False }, Cmd.none)
-      else
-        (model, Cmd.none)
-      
-    FocusNewPostEditor ->
-      ({ model | editingNewPost = True }, Cmd.none)
+        KeyDown key ->
+            if key == ctrl.keyCode || key == meta.keyCode then
+                ( { model | ctrlDown = True }, Cmd.none )
+            else
+                ( model, Cmd.none )
 
-    BlurNewPostEditor ->
-      ({ model | editingNewPost = False }, Cmd.none)
-      
-    InputNewPost content ->
-      ({ model | newPost = content }, Cmd.none)
-      
-    Post ->
-      post model
-      
-    KeyDownInNewPostEditor key ->
-      if key == enter.keyCode && model.ctrlDown then
-        post model
-      else
-        (model, Cmd.none)
+        KeyUp key ->
+            if key == ctrl.keyCode || key == meta.keyCode then
+                ( { model | ctrlDown = False }, Cmd.none )
+            else
+                ( model, Cmd.none )
 
-post : Model -> (Model, Cmd Msg)
+        FocusNewPostEditor ->
+            ( { model | editingNewPost = True }, Cmd.none )
+
+        BlurNewPostEditor ->
+            ( { model | editingNewPost = False }, Cmd.none )
+
+        InputNewPost content ->
+            ( { model | newPost = content }, Cmd.none )
+
+        Post ->
+            post model
+
+        KeyDownInNewPostEditor key ->
+            if key == enter.keyCode && model.ctrlDown then
+                post model
+            else
+                ( model, Cmd.none )
+
+
+post : Model -> ( Model, Cmd Msg )
 post model =
-  { model | posts = model.newPost :: model.posts, newPost = "" }
-    ! [Task.attempt handleScrollResult (Dom.Scroll.toBottom "timeline")]
+    { model | posts = model.newPost :: model.posts, newPost = "" }
+        ! [ Task.attempt handleScrollResult (Dom.Scroll.toBottom "timeline") ]
+
 
 handleScrollResult : Result Dom.Error () -> Msg
 handleScrollResult result =
-  case result of
-    Ok _ -> NoOp
-    Err _ -> NoOp
-            
-            
+    case result of
+        Ok _ ->
+            NoOp
+
+        Err _ ->
+            NoOp
+
+
+
 -- SUBSCRIPTIONS
+
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Sub.batch [ 
-    Keyboard.downs KeyDown, 
-    Keyboard.ups KeyUp
-  ]
-  
+    Sub.batch
+        [ Keyboard.downs KeyDown
+        , Keyboard.ups KeyUp
+        ]
+
+
 
 -- VIEW
 
+
 view : Model -> Html Msg
 view model =
-  div [id "app"] [
-    div [id "app-header"] [
-      div [class "title"] [
-        i [class "material-icons"] [text "home"]
-      ]
-    ],
-    div [id "app-body", class "container"] [
-      div [id "timeline-column", class (timelineClass model)] [
-        div [id "timeline"] 
-          (List.map (\post -> div [class "post"] [Markdown.toHtml [class "content"] post]) (List.reverse model.posts)),
-        div [id "new-post"] [
-          div [class "toolbar", hidden (not model.editingNewPost)] [
-            span [class "user"] [
-              i [class "material-icons"] [text "perm_identity"],
-              text "Anonymous"
-            ],
-            div [class "tool-buttons"] [
-              button [class "button-primary", disabled (String.isEmpty model.newPost), onMouseDown Post] [
-                text "Post",
-                span [class "shortcut-help"] [text "(Ctrl + Enter)"]
-              ]
+    div [ id "app" ]
+        [ div [ id "app-header" ]
+            [ div [ class "title" ]
+                [ i [ class "material-icons" ] [ text "home" ]
+                ]
             ]
-          ],
-          textarea[
-            class "post", 
-            placeholder "Write your idea in Markdown",
-            value model.newPost, 
-            onFocus FocusNewPostEditor, 
-            onBlur BlurNewPostEditor, 
-            onInput InputNewPost,
-            onKeyDown KeyDownInNewPostEditor
-          ] []
+        , div [ id "app-body", class "container" ]
+            [ div [ id "timeline-column", class (timelineClass model) ]
+                [ div [ id "timeline" ]
+                    (List.map (\post -> div [ class "post" ] [ Markdown.toHtml [ class "content" ] post ]) (List.reverse model.posts))
+                , div [ id "new-post" ]
+                    [ div [ class "toolbar", hidden (not model.editingNewPost) ]
+                        [ span [ class "user" ]
+                            [ i [ class "material-icons" ] [ text "perm_identity" ]
+                            , text "Anonymous"
+                            ]
+                        , div [ class "tool-buttons" ]
+                            [ button [ class "button-primary", disabled (String.isEmpty model.newPost), onMouseDown Post ]
+                                [ text "Post"
+                                , span [ class "shortcut-help" ] [ text "(Ctrl + Enter)" ]
+                                ]
+                            ]
+                        ]
+                    , textarea
+                        [ class "post"
+                        , placeholder "Write your idea in Markdown"
+                        , value model.newPost
+                        , onFocus FocusNewPostEditor
+                        , onBlur BlurNewPostEditor
+                        , onInput InputNewPost
+                        , onKeyDown KeyDownInNewPostEditor
+                        ]
+                        []
+                    ]
+                ]
+            ]
         ]
-      ]
-    ]
-  ]
+
 
 timelineClass : Model -> String
 timelineClass model =
-  if model.editingNewPost then "editing" else ""
+    if model.editingNewPost then
+        "editing"
+    else
+        ""
+
 
 onKeyDown : (Int -> msg) -> Attribute msg
 onKeyDown tagger =
-  on "keydown" (Json.map tagger keyCode)
+    on "keydown" (Json.map tagger keyCode)
--- web/elm/Keys.elm
+++ web/elm/Keys.elm
@@ -1,334 +1,474 @@
 -- Modified from https://github.com/jcollard/elm-key-constants
+
+
 module Keys exposing (..)
 
 import Keyboard
 
+
 type alias Key =
- {keyCode: Keyboard.KeyCode
- ,name: String}
+    { keyCode : Keyboard.KeyCode
+    , name : String
+    }
 
-{-| Two Keys are equal if their keyCodes are equal -}
+
+{-| Two Keys are equal if their keyCodes are equal
+-}
 equals : Key -> Key -> Bool
-equals k0 k1 = k0.keyCode == k1.keyCode
+equals k0 k1 =
+    k0.keyCode == k1.keyCode
 
-a: Key
+
+a : Key
 a =
- {keyCode = 65
- ,name = "a"}
+    { keyCode = 65
+    , name = "a"
+    }
 
-b: Key
+
+b : Key
 b =
- {keyCode = 66
- ,name = "b"}
+    { keyCode = 66
+    , name = "b"
+    }
 
-c: Key
+
+c : Key
 c =
- {keyCode = 67
- ,name = "b"}
+    { keyCode = 67
+    , name = "b"
+    }
 
-d: Key
+
+d : Key
 d =
- {keyCode = 68
- ,name = "d"}
+    { keyCode = 68
+    , name = "d"
+    }
 
-e: Key
-e = 
- {keyCode = 69
- ,name = "e"}
 
-f: Key
-f = 
- {keyCode = 70
- ,name = "f"}
+e : Key
+e =
+    { keyCode = 69
+    , name = "e"
+    }
 
-g: Key
-g = 
- {keyCode = 71
- ,name = "g"}
 
-h: Key
-h = 
- {keyCode = 72
- ,name = "h"}
+f : Key
+f =
+    { keyCode = 70
+    , name = "f"
+    }
 
-i: Key
-i = 
- {keyCode = 73
- ,name = "i"}
 
-j: Key
-j = 
- {keyCode = 74
- ,name = "j"}
+g : Key
+g =
+    { keyCode = 71
+    , name = "g"
+    }
 
-k: Key
-k = 
- {keyCode = 75
- ,name = "k"}
 
-l: Key
-l = 
- {keyCode = 76
- ,name = "l"}
+h : Key
+h =
+    { keyCode = 72
+    , name = "h"
+    }
 
-m: Key
-m = 
- {keyCode = 77
- ,name = "m"}
 
-n: Key
-n = 
- {keyCode = 78
- ,name = "n"}
+i : Key
+i =
+    { keyCode = 73
+    , name = "i"
+    }
 
-o: Key
-o = 
- {keyCode = 79
- ,name = "o"}
 
-p: Key
-p = 
- {keyCode = 80
- ,name = "p"}
+j : Key
+j =
+    { keyCode = 74
+    , name = "j"
+    }
 
-q: Key
-q = 
- {keyCode = 81
- ,name = "q"}
 
-r: Key
-r = 
- {keyCode = 82
- ,name = "r"}
+k : Key
+k =
+    { keyCode = 75
+    , name = "k"
+    }
 
-s: Key
-s = 
- {keyCode = 83
- ,name = "s"}
 
-t: Key
-t = 
- {keyCode = 84
- ,name = "t"}
+l : Key
+l =
+    { keyCode = 76
+    , name = "l"
+    }
 
-u: Key
-u = 
- {keyCode = 85
- ,name = "u"}
 
-v: Key
-v = 
- {keyCode = 86
- ,name = "v"}
+m : Key
+m =
+    { keyCode = 77
+    , name = "m"
+    }
 
-w: Key
-w = 
- {keyCode = 87
- ,name = "w"}
 
-x: Key
-x = 
- {keyCode = 88
- ,name = "x"}
+n : Key
+n =
+    { keyCode = 78
+    , name = "n"
+    }
 
-y: Key
-y = 
- {keyCode = 89
- ,name = "y"}
 
-z: Key
-z = 
- {keyCode = 90
- ,name = "z"}
+o : Key
+o =
+    { keyCode = 79
+    , name = "o"
+    }
 
-ctrl: Key
-ctrl = 
- {keyCode = 17
- ,name = "Ctrl"}
 
-shift: Key
-shift = 
- {keyCode = 16
- ,name = "Shift"}
+p : Key
+p =
+    { keyCode = 80
+    , name = "p"
+    }
 
-tab: Key
-tab = 
- {keyCode = 9
- ,name = "Tab"}
 
-{-| super,meta,windows are all the same -}
-super: Key
-super = 
- {keyCode = 91
- ,name = "Super"}
+q : Key
+q =
+    { keyCode = 81
+    , name = "q"
+    }
 
-{-| super,meta,windows are all the same -}
-meta: Key
-meta = 
- {keyCode = 91
- ,name = "Meta"}
 
-{-| super,meta,windows are all the same -}
-windows: Key
-windows = 
- {keyCode = 91
- ,name = "Windows"}
+r : Key
+r =
+    { keyCode = 82
+    , name = "r"
+    }
 
-{-| A key on mac keyboards. The same keycode as the windows/super/meta keys -}
-commandLeft: Key
+
+s : Key
+s =
+    { keyCode = 83
+    , name = "s"
+    }
+
+
+t : Key
+t =
+    { keyCode = 84
+    , name = "t"
+    }
+
+
+u : Key
+u =
+    { keyCode = 85
+    , name = "u"
+    }
+
+
+v : Key
+v =
+    { keyCode = 86
+    , name = "v"
+    }
+
+
+w : Key
+w =
+    { keyCode = 87
+    , name = "w"
+    }
+
+
+x : Key
+x =
+    { keyCode = 88
+    , name = "x"
+    }
+
+
+y : Key
+y =
+    { keyCode = 89
+    , name = "y"
+    }
+
+
+z : Key
+z =
+    { keyCode = 90
+    , name = "z"
+    }
+
+
+ctrl : Key
+ctrl =
+    { keyCode = 17
+    , name = "Ctrl"
+    }
+
+
+shift : Key
+shift =
+    { keyCode = 16
+    , name = "Shift"
+    }
+
+
+tab : Key
+tab =
+    { keyCode = 9
+    , name = "Tab"
+    }
+
+
+{-| super,meta,windows are all the same
+-}
+super : Key
+super =
+    { keyCode = 91
+    , name = "Super"
+    }
+
+
+{-| super,meta,windows are all the same
+-}
+meta : Key
+meta =
+    { keyCode = 91
+    , name = "Meta"
+    }
+
+
+{-| super,meta,windows are all the same
+-}
+windows : Key
+windows =
+    { keyCode = 91
+    , name = "Windows"
+    }
+
+
+{-| A key on mac keyboards. The same keycode as the windows/super/meta keys
+-}
+commandLeft : Key
 commandLeft =
- {keyCode = 91
- ,name = "Command left"}
+    { keyCode = 91
+    , name = "Command left"
+    }
 
-{-| A key on mac keyboards. -}
-commandRight: Key
+
+{-| A key on mac keyboards.
+-}
+commandRight : Key
 commandRight =
- {keyCode = 93
- ,name = "Command right"}
+    { keyCode = 93
+    , name = "Command right"
+    }
 
-space: Key
-space = 
- {keyCode = 32
- ,name = "Space"}
 
-enter: Key
-enter = 
- {keyCode = 13
- ,name = "Enter"}
+space : Key
+space =
+    { keyCode = 32
+    , name = "Space"
+    }
 
-arrowRight: Key
-arrowRight = 
- {keyCode = 37
- ,name = "Right arrow"}
 
-arrowLeft: Key
-arrowLeft = 
- {keyCode = 39
- ,name = "Left arrow"}
+enter : Key
+enter =
+    { keyCode = 13
+    , name = "Enter"
+    }
 
-arrowUp: Key
-arrowUp = 
- {keyCode = 38
- ,name = "Up arrow"}
 
-arrowDown: Key
-arrowDown = 
- {keyCode = 40
- ,name = "Down arrow"}
+arrowRight : Key
+arrowRight =
+    { keyCode = 37
+    , name = "Right arrow"
+    }
 
-backspace: Key
-backspace = 
- {keyCode = 8
- ,name = "Backspace"}
 
-delete: Key
-delete = 
- {keyCode = 46
- ,name = "Delete"}
+arrowLeft : Key
+arrowLeft =
+    { keyCode = 39
+    , name = "Left arrow"
+    }
 
-insert: Key
-insert = 
- {keyCode = 45
- ,name = "Insert"}
 
-end: Key
-end = 
- {keyCode = 35
- ,name = "End"}
+arrowUp : Key
+arrowUp =
+    { keyCode = 38
+    , name = "Up arrow"
+    }
 
-home: Key
-home = 
- {keyCode = 36
- ,name = "Home"}
 
-pageDown: Key
-pageDown = 
- {keyCode = 34
- ,name = "Page down"}
+arrowDown : Key
+arrowDown =
+    { keyCode = 40
+    , name = "Down arrow"
+    }
 
-pageUp: Key
-pageUp = 
- {keyCode = 33
- ,name = "Page up"}
 
-escape: Key
-escape = 
- {keyCode = 27
- ,name = "Escape"}
+backspace : Key
+backspace =
+    { keyCode = 8
+    , name = "Backspace"
+    }
+
+
+delete : Key
+delete =
+    { keyCode = 46
+    , name = "Delete"
+    }
+
+
+insert : Key
+insert =
+    { keyCode = 45
+    , name = "Insert"
+    }
+
+
+end : Key
+end =
+    { keyCode = 35
+    , name = "End"
+    }
+
+
+home : Key
+home =
+    { keyCode = 36
+    , name = "Home"
+    }
+
+
+pageDown : Key
+pageDown =
+    { keyCode = 34
+    , name = "Page down"
+    }
+
+
+pageUp : Key
+pageUp =
+    { keyCode = 33
+    , name = "Page up"
+    }
+
+
+escape : Key
+escape =
+    { keyCode = 27
+    , name = "Escape"
+    }
+
+
 
 -- We don't define the F keys that are not availiable.  AKA, F1 is help, F3 is search.  F5 is refresh. Those keys cannot be used.
 
-f2: Key
-f2 = 
- {keyCode = 113
- ,name = "F2"}
 
-f4: Key
-f4 = 
- {keyCode = 115
- ,name = "F4"}
+f2 : Key
+f2 =
+    { keyCode = 113
+    , name = "F2"
+    }
 
-f8: Key
-f8 = 
- {keyCode = 119
- ,name = "F8"}
 
-f9: Key
-f9 = 
- {keyCode = 120
- ,name = "F9"}
+f4 : Key
+f4 =
+    { keyCode = 115
+    , name = "F4"
+    }
 
-f10: Key
-f10 = 
- {keyCode = 121
- ,name = "F10"}
 
-one: Key
-one = 
- {keyCode = 49
- ,name = "1"}
+f8 : Key
+f8 =
+    { keyCode = 119
+    , name = "F8"
+    }
 
-two: Key
-two = 
- {keyCode = 50
- ,name = "2"}
 
-three: Key
-three = 
- {keyCode = 51
- ,name = "3"}
+f9 : Key
+f9 =
+    { keyCode = 120
+    , name = "F9"
+    }
 
-four: Key
-four = 
- {keyCode = 52
- ,name = "4"}
 
-five: Key
-five = 
- {keyCode = 53
- ,name = "5"}
+f10 : Key
+f10 =
+    { keyCode = 121
+    , name = "F10"
+    }
 
-six: Key
-six = 
- {keyCode = 54
- ,name = "6"}
 
-seven: Key
-seven = 
- {keyCode = 55
- ,name = "7"}
+one : Key
+one =
+    { keyCode = 49
+    , name = "1"
+    }
 
-eight: Key
-eight = 
- {keyCode = 56
- ,name = "8"}
 
-nine: Key
-nine = 
- {keyCode = 57
- ,name = "9"}
+two : Key
+two =
+    { keyCode = 50
+    , name = "2"
+    }
 
-zero: Key
-zero = 
- {keyCode = 58
- ,name = "0"}
+
+three : Key
+three =
+    { keyCode = 51
+    , name = "3"
+    }
+
+
+four : Key
+four =
+    { keyCode = 52
+    , name = "4"
+    }
+
+
+five : Key
+five =
+    { keyCode = 53
+    , name = "5"
+    }
+
+
+six : Key
+six =
+    { keyCode = 54
+    , name = "6"
+    }
+
+
+seven : Key
+seven =
+    { keyCode = 55
+    , name = "7"
+    }
+
+
+eight : Key
+eight =
+    { keyCode = 56
+    , name = "8"
+    }
+
+
+nine : Key
+nine =
+    { keyCode = 57
+    , name = "9"
+    }
+
+
+zero : Key
+zero =
+    { keyCode = 58
+    , name = "0"
+    }
