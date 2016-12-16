3e5fd487ca73a21897ba148cf086e4f8b5ae9c64
Daisuke Morita <daisuke.marubinotto@gmail.com>
2016-12-16 18:31:06 +0900

    Create Cotoami.SigninController

---
+++ web/controllers/signin_controller.ex
@@ -0,0 +1,11 @@
+defmodule Cotoami.SigninController do
+  use Cotoami.Web, :controller
+  
+  def request(conn, %{"email" => email}) do
+    
+  end
+  
+  def signin(conn, %{"token" => token, "anonymous_id" => anonymous_id}) do
+    
+  end
+end
