6b6ba8a6cdf7201a5a3e64c034b7e9af164629f8
Daisuke Morita <daisuke.marubinotto@gmail.com>
2016-12-19 18:12:23 +0900

    Refactor web/controllers/auth.ex

--- web/controllers/auth.ex
+++ web/controllers/auth.ex
@@ -3,7 +3,7 @@ defmodule Cotoami.Auth do
   require Logger
   
   @cookie_for_anonymous_id "cotoami_anonymous_id"
-  @cookie_max_age 60 * 60 * 24 * 365 * 10
+  @anonymous_max_age 60 * 60 * 24 * 365 * 10
   
   def init(options) do
     options
@@ -12,9 +12,9 @@ defmodule Cotoami.Auth do
   def call(conn, _opts) do
     case get_anonymous_id(conn) do
       nil ->
-        new_id = generate_id
+        new_id = generate_anonymous_id
         conn
-        |> put_resp_cookie(@cookie_for_anonymous_id, new_id, max_age: @cookie_max_age)
+        |> put_resp_cookie(@cookie_for_anonymous_id, new_id, max_age: @anonymous_max_age)
         |> assign_anonymous_id(new_id) 
       id ->
         conn |> assign_anonymous_id(id) 
@@ -25,8 +25,9 @@ defmodule Cotoami.Auth do
     conn.cookies[@cookie_for_anonymous_id]
   end
   
-  defp generate_id do
-    :crypto.strong_rand_bytes(30) |> Base.hex_encode32(case: :lower)
+  defp generate_anonymous_id do
+    "anonymous-" <> 
+      (:crypto.strong_rand_bytes(30) |> Base.hex_encode32(case: :lower))
   end
   
   defp assign_anonymous_id(conn, anonymous_id) do
