55619cf57443ff8d62dbfa7d9007ced2cfe6239f
Daisuke Morita <daisuke.marubinotto@gmail.com>
2016-12-27 16:38:53 +0900

    Rename App.elm to Main.elm

--- brunch-config.js
+++ brunch-config.js
@@ -54,7 +54,7 @@ exports.config = {
   plugins: {
     elmBrunch: {
       elmFolder: "web/elm",
-      mainModules: ["App.elm"],
+      mainModules: ["Main.elm"],
       outputFolder: "../static/vendor"
     },
     babel: {
