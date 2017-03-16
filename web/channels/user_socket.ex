86a48e6db9fe42430d10f9e7474fe0fbe8ba7ba4
Daisuke Morita <daisuke.marubinotto@gmail.com>
2017-03-16 09:53:31 +0900

    Create Cotoami.CotonomaChannel

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -1,37 +1,26 @@
 defmodule Cotoami.UserSocket do
   use Phoenix.Socket
+  alias Cotoami.Repo
 
   ## Channels
-  # channel "room:*", Cotoami.RoomChannel
+  channel "cotonomas:*", Cotoami.CotonomaChannel
 
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket
   # transport :longpoll, Phoenix.Transports.LongPoll
+  
+  @max_age 2 * 7 * 24 * 60 * 60
 
-  # Socket params are passed from the client and can
-  # be used to verify and authenticate a user. After
-  # verification, you can put default assigns into
-  # the socket that will be set for all channels, ie
-  #
-  #     {:ok, assign(socket, :user_id, verified_user_id)}
-  #
-  # To deny connection, return `:error`.
-  #
-  # See `Phoenix.Token` documentation for examples in
-  # performing token verification on connect.
-  def connect(_params, socket) do
-    {:ok, socket}
+  def connect(%{"token" => token}, socket) do
+    case Phoenix.Token.verify(socket, "amishi", token, max_age: @max_age) do
+      {:ok, amishi_id} ->
+        amishi = Repo.get!(Cotoami.Amishi, amishi_id)
+        {:ok, assign(socket, :amishi, amishi)}
+      {:error, _reason} ->
+        :error
+    end
   end
+  def connect(_params, _socket), do: :error
 
-  # Socket id's are topics that allow you to identify all sockets for a given user:
-  #
-  #     def id(socket), do: "users_socket:#{socket.assigns.user_id}"
-  #
-  # Would allow you to broadcast a "disconnect" event and terminate
-  # all active sockets and channels for a given user:
-  #
-  #     Cotoami.Endpoint.broadcast("users_socket:#{user.id}", "disconnect", %{})
-  #
-  # Returning `nil` makes this socket anonymous.
-  def id(_socket), do: nil
+  def id(socket), do: "users_socket:#{socket.assigns.amishi.id}"
 end
