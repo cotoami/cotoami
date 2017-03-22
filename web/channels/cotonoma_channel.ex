5faa46eb10696947b0f0d262ffe9e9f3c5af8f6f
Daisuke Morita <daisuke.marubinotto@gmail.com>
2017-03-22 13:37:37 +0900

    Add a comment

--- web/channels/cotonoma_channel.ex
+++ web/channels/cotonoma_channel.ex
@@ -2,6 +2,7 @@ defmodule Cotoami.CotonomaChannel do
   use Cotoami.Web, :channel
   
   def join("cotonomas:" <> cotonoma_key, _params, socket) do
+    # TODO: check permission
     {:ok, socket}
   end
 end
