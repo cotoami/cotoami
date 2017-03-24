277f02a0ed875431a64aac834955972f820cf6f9
Daisuke Morita <daisuke.marubinotto@gmail.com>
2017-03-24 17:52:21 +0900

    Fix a bug

--- web/channels/cotonoma_channel.ex
+++ web/channels/cotonoma_channel.ex
@@ -3,6 +3,7 @@ defmodule Cotoami.CotonomaChannel do
   alias Cotoami.CotonomaService
   
   def join("cotonomas:" <> cotonoma_key, _params, socket) do
+    amishi_id = socket.assigns.amishi.id
     case CotonomaService.get_by_key(cotonoma_key, amishi_id) do
       nil ->
         {:error, %{reason: "unauthorized"}}
