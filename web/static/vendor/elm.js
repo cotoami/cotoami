bf1503a084b7e89e978e0258dc561a440d8f3458
Daisuke Morita <daisuke.marubinotto@gmail.com>
2017-03-29 15:30:36 +0900

    Create connections.scss

--- web/static/vendor/elm.js
+++ web/static/vendor/elm.js
@@ -25284,7 +25284,92 @@ var _user$project$Components_Connections_View$view = function (model) {
 		},
 		{
 			ctor: '::',
-			_0: _elm_lang$html$Html$text('connections'),
+			_0: A2(
+				_elm_lang$html$Html$div,
+				{
+					ctor: '::',
+					_0: _elm_lang$html$Html_Attributes$id('root-connections'),
+					_1: {ctor: '[]'}
+				},
+				{
+					ctor: '::',
+					_0: A2(
+						_elm_lang$html$Html$div,
+						{
+							ctor: '::',
+							_0: _elm_lang$html$Html_Attributes$class('coto'),
+							_1: {ctor: '[]'}
+						},
+						{
+							ctor: '::',
+							_0: A2(
+								_elm_lang$html$Html$div,
+								{
+									ctor: '::',
+									_0: _elm_lang$html$Html_Attributes$class('content'),
+									_1: {ctor: '[]'}
+								},
+								{
+									ctor: '::',
+									_0: _elm_lang$html$Html$text('Is is possible to attach additional security groups to the rules for the node security group? Our use case is that we want to deploy Kubernetes into an existing VPC. The Kubernetes VPC is peered with another VPC that holds things like RDS instances and long-lived EC2 sysstems and want the Kubernetes nodes to be able to communicate with these systems by basically creating an ALLOW ALL set of ingress and egress rules.'),
+									_1: {ctor: '[]'}
+								}),
+							_1: {ctor: '[]'}
+						}),
+					_1: {
+						ctor: '::',
+						_0: A2(
+							_elm_lang$html$Html$div,
+							{
+								ctor: '::',
+								_0: _elm_lang$html$Html_Attributes$class('coto'),
+								_1: {ctor: '[]'}
+							},
+							{
+								ctor: '::',
+								_0: A2(
+									_elm_lang$html$Html$div,
+									{
+										ctor: '::',
+										_0: _elm_lang$html$Html_Attributes$class('content'),
+										_1: {ctor: '[]'}
+									},
+									{
+										ctor: '::',
+										_0: _elm_lang$html$Html$text('Kubernetes'),
+										_1: {ctor: '[]'}
+									}),
+								_1: {ctor: '[]'}
+							}),
+						_1: {
+							ctor: '::',
+							_0: A2(
+								_elm_lang$html$Html$div,
+								{
+									ctor: '::',
+									_0: _elm_lang$html$Html_Attributes$class('coto'),
+									_1: {ctor: '[]'}
+								},
+								{
+									ctor: '::',
+									_0: A2(
+										_elm_lang$html$Html$div,
+										{
+											ctor: '::',
+											_0: _elm_lang$html$Html_Attributes$class('content'),
+											_1: {ctor: '[]'}
+										},
+										{
+											ctor: '::',
+											_0: _elm_lang$html$Html$text('ODLEは有機化合物を用いた発光素子で、液晶（LCD）などに比べ薄型で軽量、かつ低消費電力、高速応答、高コントラストなどの特徴があります。サムスンディスプレイでは、アクティブマトリクスという駆動方式を採用したAMOLED(Active Matrix Organic Light Emitting Diode)を主力にしています。AMOLEDは、Apple Watchやサムスンギャラクシーシリーズに搭載されています。変形が用意であるため、スマホ表示面のエッジを曲げることなども可能です。'),
+											_1: {ctor: '[]'}
+										}),
+									_1: {ctor: '[]'}
+								}),
+							_1: {ctor: '[]'}
+						}
+					}
+				}),
 			_1: {ctor: '[]'}
 		});
 };
