ac0f121e8b698f9cac5744332d67bc81bc8f9357
Daisuke Morita <daisuke.marubinotto@gmail.com>
2017-04-20 23:03:26 +0900

    Add implementation to Cotoami.ApiCsrfProtection

--- web/plugs/api_csrf_protection.ex
+++ web/plugs/api_csrf_protection.ex
@@ -3,7 +3,7 @@ defmodule Cotoami.ApiCsrfProtection do
   require Logger
   
   @unprotected_methods ~w(HEAD GET OPTIONS)
-  @custom_header "X-From"
+  @custom_header "x-requested-with"
   
   defmodule InvalidApiRequestError do
     defexception message: "invalid api request", plug_status: 403
@@ -25,13 +25,55 @@ defmodule Cotoami.ApiCsrfProtection do
   defp verified_request?(conn) do
     conn.method in @unprotected_methods
       || not cookie_authentication?(conn)
+      || csrf_safe?(conn)
   end
   
-  defp cookie_authentication?(conn) do
+  defp cookie_authentication?(_conn) do
     true
   end
   
-  defp xhr?(conn) do
-    "XMLHttpRequest" in get_req_header(conn, "x-requested-with")
+  defp csrf_safe?(conn) do
+    valid_host?(conn)
+      && custom_header_exists?(conn)
+      && valid_origin?(conn)
+  end
+  
+  # DNS rebinding attack prevention
+  defp valid_host?(conn) do
+    %{host: host, port: port} = Cotoami.Endpoint.struct_url()
+    host_and_port = "#{host}:#{port}"
+    case get_req_header(conn, "host") do
+      [^host] ->
+        true
+      [^host_and_port] ->
+        true
+      [invalid_host] ->
+        Logger.info "invalid host header: #{invalid_host}"
+        false
+    end
+  end
+  
+  # Ensure the request has been sent via xhr
+  # http://blog.alutam.com/2011/09/14/jersey-and-cross-site-request-forgery-csrf/
+  defp custom_header_exists?(conn) do
+    case get_req_header(conn, @custom_header) do
+      [_value] ->
+        true
+      _ ->
+        Logger.info "custom header '#{@custom_header}' does not exist"
+        false
+    end
+  end
+  
+  # Ensure the xhr request came from the same origin
+  defp valid_origin?(conn) do
+    case get_req_header(conn, "origin") do
+      [origin] ->
+        Logger.info "origin header: #{origin}"
+        %{scheme: scheme, host: host} = Cotoami.Endpoint.struct_url()
+        String.starts_with?(origin, "#{scheme}://#{host}")
+      _ ->
+        true
+    end
   end
 end
