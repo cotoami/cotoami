860e89cd07977fed8c8290101476cf97082926a9
Daisuke Morita <daisuke.marubinotto@gmail.com>
2017-06-25 17:23:09 +0900

    Refactoring: extract Components.CotoSelection.View

--- elm/App/View.elm
+++ elm/App/View.elm
@@ -13,6 +13,7 @@ import Components.ConfirmModal.View
 import Components.SigninModal
 import Components.ProfileModal
 import Components.CotoModal
+import Components.CotoSelection.View exposing (cotoSelectionTools)
 import Components.Timeline.View
 import Components.CotonomaModal.View
 import Components.Pinned.View
@@ -124,45 +125,6 @@ defaultColumnDivs model =
             model.graph
         ]
     ]
-    
-
-cotoSelectionTools : Model -> Html Msg
-cotoSelectionTools model =
-    if List.isEmpty model.cotoSelection then
-        div [] []
-    else
-        div [ id "coto-selection-tools" ] 
-            [ a [ class "close", onClick ClearSelection ] 
-                [ i [ class "fa fa-times", (attribute "aria-hidden" "true") ] [] ] 
-            , if model.connectMode then
-                div [ class "connect-mode" ]
-                    [ span 
-                        [ class "connect-mode-message" ] 
-                        [ text "Select a target coto..." ]
-                    , button 
-                        [ class "button", onClick (SetConnectMode False) ] 
-                        [ text "Cancel" ]
-                    ]
-              else
-                div [ class "default" ]
-                    [ div [ class "selection-info" ]
-                        [ span 
-                            [ class "selection-count" ] 
-                            [ text (model.cotoSelection |> List.length |> toString) ]
-                        , text " cotos selected"
-                        ]
-                    , div [ class "buttons" ]
-                        [ button 
-                           [ class "button", onClick Pin ] 
-                           [ i [ class "fa fa-thumb-tack", (attribute "aria-hidden" "true") ] []
-                           , text "Pin" 
-                           ]
-                        , button 
-                           [ class "button", onClick (SetConnectMode True) ] 
-                           [ text "Connect" ]
-                        ]
-                    ]
-            ]
             
             
 viewSwitchContainerDiv : Model -> Html Msg
---
+++ elm/Components/CotoSelection/View.elm
@@ -0,0 +1,46 @@
+module Components.CotoSelection.View exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
+import App.Model exposing (..)
+import App.Messages exposing (..)
+
+
+cotoSelectionTools : Model -> Html Msg
+cotoSelectionTools model =
+    if List.isEmpty model.cotoSelection then
+        div [] []
+    else
+        div [ id "coto-selection-tools" ] 
+            [ a [ class "close", onClick ClearSelection ] 
+                [ i [ class "fa fa-times", (attribute "aria-hidden" "true") ] [] ] 
+            , if model.connectMode then
+                div [ class "connect-mode" ]
+                    [ span 
+                        [ class "connect-mode-message" ] 
+                        [ text "Select a target coto..." ]
+                    , button 
+                        [ class "button", onClick (SetConnectMode False) ] 
+                        [ text "Cancel" ]
+                    ]
+              else
+                div [ class "default" ]
+                    [ div [ class "selection-info" ]
+                        [ span 
+                            [ class "selection-count" ] 
+                            [ text (model.cotoSelection |> List.length |> toString) ]
+                        , text " cotos selected"
+                        ]
+                    , div [ class "buttons" ]
+                        [ button 
+                           [ class "button", onClick Pin ] 
+                           [ i [ class "fa fa-thumb-tack", (attribute "aria-hidden" "true") ] []
+                           , text "Pin" 
+                           ]
+                        , button 
+                           [ class "button", onClick (SetConnectMode True) ] 
+                           [ text "Connect" ]
+                        ]
+                    ]
+            ]
