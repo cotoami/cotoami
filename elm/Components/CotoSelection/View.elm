114ba43a8e37c2a8d15c357fa4f9db5f70e34ac1
Daisuke Morita <daisuke.marubinotto@gmail.com>
2017-06-25 23:02:25 +0900

    Connect mode from #main-selection column

--- elm/Components/CotoSelection/View.elm
+++ elm/Components/CotoSelection/View.elm
@@ -45,7 +45,7 @@ cotoSelectionToolsDiv =
           , text "Pin" 
           ]
       , button 
-          [ class "button" ] 
+          [ class "button", onClick (SetConnectMode True) ] 
           [ text "Connect" ]
       , span 
           [ class "group-title" ]
@@ -110,33 +110,35 @@ bodyDiv graph coto =
 
 cotoSelectionTools : Model -> Html Msg
 cotoSelectionTools model =
-    if List.isEmpty model.cotoSelection then
-        div [] []
-    else
-        div [ id "coto-selection-tools" ] 
-            [ a [ class "close", onClick ClearSelection ] 
-                [ i [ class "fa fa-times", (attribute "aria-hidden" "true") ] [] ] 
-            , if model.connectMode then
-                div [ class "connect-mode" ]
-                    [ span 
-                        [ class "connect-mode-message" ] 
-                        [ text "Select a target coto..." ]
-                    , button 
-                        [ class "button", onClick (SetConnectMode False) ] 
-                        [ text "Cancel" ]
-                    ]
-              else
-                div [ class "default" ]
-                    [ selectionInfoDiv model
-                    , div [ class "buttons" ]
-                        [ button 
-                           [ class "button", onClick ConfirmPin ] 
-                           [ i [ class "fa fa-thumb-tack", (attribute "aria-hidden" "true") ] []
-                           , text "Pin" 
-                           ]
-                        , button 
-                           [ class "button", onClick (SetConnectMode True) ] 
-                           [ text "Connect" ]
-                        ]
-                    ]
+    div [ id "coto-selection-tools"
+        , classList
+            [ ( "empty", List.isEmpty model.cotoSelection )
+            , ( "in-connect-mode", model.connectMode )
             ]
+        ] 
+        [ a [ class "close", onClick ClearSelection ] 
+            [ i [ class "fa fa-times", (attribute "aria-hidden" "true") ] [] ] 
+        , if model.connectMode then
+            div [ class "connect-mode" ]
+                [ span 
+                    [ class "connect-mode-message" ] 
+                    [ text "Select a target coto..." ]
+                , button 
+                    [ class "button", onClick (SetConnectMode False) ] 
+                    [ text "Cancel" ]
+                ]
+          else
+            div [ class "default" ]
+                [ selectionInfoDiv model
+                , div [ class "buttons" ]
+                    [ button 
+                       [ class "button", onClick ConfirmPin ] 
+                       [ i [ class "fa fa-thumb-tack", (attribute "aria-hidden" "true") ] []
+                       , text "Pin" 
+                       ]
+                    , button 
+                       [ class "button", onClick (SetConnectMode True) ] 
+                       [ text "Connect" ]
+                    ]
+                ]
+        ]
