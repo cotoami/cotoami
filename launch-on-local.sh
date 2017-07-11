71f1c43976e6f007cfb7aa6b6c1f744c194491ca
Daisuke Morita <daisuke.marubinotto@gmail.com>
2017-07-11 18:45:26 +0900

    Refactor launch-on-local.sh: extract launch-backends-on-local.sh

--- launch-on-local.sh
+++ launch-on-local.sh
@@ -1,51 +1,15 @@
 #!/bin/bash
 
-#
 # Get dependencies
-#
-
 echo
 echo "# Getting dependencies..."
 mix do deps.get, deps.compile
 npm install
 
+# Launch backend services
+source ./launch-backends-on-local.sh
 
-#
-# Run backend services as docker containers
-#
-
-if [ -n "$DOCKER_HOST" ]; then
-  DOCKER_HOST_IP=$(echo $DOCKER_HOST | sed 's/^.*\/\/\(.*\):[0-9][0-9]*$/\1/g')
-else
-  DOCKER_HOST_IP="127.0.0.1"
-fi
-
-# Redis
-echo
-echo "# Running redis..."
-docker run -d -p 6379:6379 redis:alpine
-export COTOAMI_REDIS_HOST=$DOCKER_HOST_IP
-
-# PostgreSQL
-echo
-echo "# Running postgres..."
-docker run -d -p 5432:5432 -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=cotoami_dev postgres:alpine
-export COTOAMI_DEV_REPO_HOST=$DOCKER_HOST_IP
-
-# Mail server
-echo
-echo "# Running maildev..."
-docker run -d -p 25:25 -p 8080:80 djfarrelly/maildev:latest
-export COTOAMI_SMTP_SERVER=$DOCKER_HOST_IP
-export COTOAMI_SMTP_PORT=25
-echo
-echo "You can check sign-up/in mails at http://$DOCKER_HOST_IP:8080"
-
-
-#
 # Launch app
-#
-
 echo
 echo "# Launching cotoami..."
 mix phoenix.server
