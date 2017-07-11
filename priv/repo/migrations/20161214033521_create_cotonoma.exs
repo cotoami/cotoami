2060ec6630b63e85d480ddc737f0e21a3eab7dab
Daisuke Morita <daisuke.marubinotto@gmail.com>
2017-07-11 19:17:35 +0900

    Incorporate minor migrations into 'create table' migrations

--- priv/repo/migrations/20161214033521_create_cotonoma.exs
+++ priv/repo/migrations/20161214033521_create_cotonoma.exs
@@ -4,14 +4,15 @@ defmodule Cotoami.Repo.Migrations.CreateCotonoma do
   def change do
     create table(:cotonomas) do
       add :key, :string, null: false
-      add :name, :string, null: true
-      add :coto_id, references(:cotos, on_delete: :delete_all), null: true
+      add :name, :string, null: false
+      add :coto_id, references(:cotos, on_delete: :delete_all), null: false
       add :owner_id, references(:amishis, on_delete: :delete_all), null: false
 
       timestamps()
     end
-    
+
     create unique_index(:cotonomas, [:key])
+    create unique_index(:cotonomas, [:name, :owner_id], name: :cotonomas_name_owner_id_index)
     create index(:cotonomas, [:coto_id])
     create index(:cotonomas, [:owner_id])
   end
--- priv/repo/migrations/20170129153759_make_cotonoma_name_not_nullable.exs
+++
@@ -1,9 +0,0 @@
-defmodule Cotoami.Repo.Migrations.MakeCotonomaNameNotNullable do
-  use Ecto.Migration
-
-  def change do
-    alter table(:cotonomas) do
-      modify :name, :string, null: false
-    end
-  end
-end
