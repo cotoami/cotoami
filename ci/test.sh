63dbabe3fb53d398290b0a83951b42bff3fbd141
Daisuke Morita <daisuke.marubinotto@gmail.com>
2017-07-14 18:22:07 +0900

    Move test commands in circle.yml to ci/test.sh

--- ci/test.sh
+++ ci/test.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-export MIX_ENV="test"
-mix do deps.get, deps.compile, compile, test
+source "${BASH_SOURCE%/*}/setup-test.sh"
+
+docker run --net=host -v `pwd`:/build -v /etc/localtime:/etc/localtime:ro -i -t \
+  -e COTOAMI_REDIS_HOST \
+  -e COTOAMI_REDIS_PORT \
+  -e COTOAMI_TEST_REPO_HOST \
+  -e COTOAMI_TEST_REPO_PORT \
+  -e COTOAMI_NEO4J_HOST \
+  -e COTOAMI_NEO4J_PORT \
+  elixir /bin/sh -c 'cd /build && MIX_ENV="test" mix do deps.get, deps.compile, compile, test'
