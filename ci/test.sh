7cac71687ff44df7a1b51a29472360d3c14fbd0d
Daisuke Morita <daisuke.marubinotto@gmail.com>
2017-07-14 22:12:19 +0900

    circle.yml: move test setup to the dependencies section in order for saved docker images to be cached

--- ci/test.sh
+++ ci/test.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-source "${BASH_SOURCE%/*}/setup-test.sh"
-
 docker run --net=host -v `pwd`:/build -v /etc/localtime:/etc/localtime:ro -i -t \
-  -e COTOAMI_REDIS_HOST \
-  -e COTOAMI_REDIS_PORT \
-  -e COTOAMI_TEST_REPO_HOST \
-  -e COTOAMI_TEST_REPO_PORT \
-  -e COTOAMI_NEO4J_HOST \
-  -e COTOAMI_NEO4J_PORT \
+  --env-file ~/test-env-vars
   elixir /bin/sh -c 'cd /build && MIX_ENV="test" mix do deps.get, deps.compile, compile, test'
