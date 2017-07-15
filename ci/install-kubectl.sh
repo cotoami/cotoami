5581b4dfa00fe3554ba2ff679364903ff7076472
Daisuke Morita <daisuke.marubinotto@gmail.com>
2017-07-16 02:20:38 +0900

    Use curl instead of wget in install-kubectl.sh

--- ci/install-kubectl.sh
+++ ci/install-kubectl.sh
@@ -3,7 +3,7 @@
 set -ex
 
 apt-get -y -qq update
-apt-get -y -qq install sudo wget jq gettext python3.4-dev
+apt-get -y -qq install sudo curl jq gettext python3.4-dev
 
 # awscli
 curl -O https://bootstrap.pypa.io/get-pip.py
@@ -12,11 +12,11 @@ pip install awscli --upgrade --user
 
 # install kubectl
 if [ ! -e ~/.kube ]; then
-    mkdir -p ~/.kube;
+  mkdir -p ~/.kube;
 fi
 if [ ! -e ~/.kube/kubectl ]; then
-    wget https://storage.googleapis.com/kubernetes-release/release/v${KUBE_VERSION}/bin/linux/amd64/kubectl -O ~/.kube/kubectl
-    chmod +x ~/.kube/kubectl
+  curl Ss -L https://storage.googleapis.com/kubernetes-release/release/v${KUBE_VERSION}/bin/linux/amd64/kubectl -o ~/.kube/kubectl
+  chmod +x ~/.kube/kubectl
 fi
 
 aws s3 cp ${S3_KUBE_CONF} ~/.kube/config
