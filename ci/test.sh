e0c1d61f5fa00cadd263c6233ca5a88d5ba53226
Daisuke Morita <daisuke.marubinotto@gmail.com>
2017-07-15 14:49:42 +0900

    Refactor ci/test.sh

--- ci/test.sh
+++ ci/test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-docker run --net=host -v `pwd`:/build -v /etc/localtime:/etc/localtime:ro -i -t \
+docker run --net=host  -i -t \
+  -v `pwd`:/build \
+  -v /etc/localtime:/etc/localtime:ro \
   --env-file ~/test-env-vars \
   elixir /bin/sh -c 'cd /build && MIX_ENV="test" mix do deps.get, deps.compile, compile, test'
