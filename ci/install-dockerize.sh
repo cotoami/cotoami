1448c371bae7ace560ebd5334ef1dbdb928f8732
Daisuke Morita <daisuke.marubinotto@gmail.com>
2017-07-15 20:13:36 +0900

    Fix dockerize installation script

--- ci/install-dockerize.sh
+++ ci/install-dockerize.sh
@@ -3,6 +3,6 @@
 DOCKERIZE_VERSION="v0.3.0"
 DOCKERIZE_DOWNLOAD_URL=https://github.com/jwilder/dockerize/releases/download/$DOCKERIZE_VERSION/dockerize-linux-amd64-$DOCKERIZE_VERSION.tar.gz
 
-curl DOCKERIZE_DOWNLOAD_URL -o dockerize.tar.gz
+curl $DOCKERIZE_DOWNLOAD_URL -o dockerize.tar.gz
 sudo tar -C /usr/local/bin -xzvf dockerize.tar.gz
 rm dockerize.tar.gz
