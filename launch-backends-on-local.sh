ca0310fc899bbfb7cf44b6b12d4bc3efe418a4ef
Daisuke Morita <daisuke.marubinotto@gmail.com>
2017-07-30 21:00:45 +0900

    Add neo4j backend to launch-backends-on-local.sh

--- launch-backends-on-local.sh
+++ launch-backends-on-local.sh
@@ -22,6 +22,12 @@ echo "# Running postgres..."
 docker run -d -p 5432:5432 -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=cotoami_dev postgres:9.5-alpine
 export COTOAMI_DEV_REPO_HOST=$DOCKER_HOST_IP
 
+# Neo4j
+echo
+echo "# Running neo4j..."
+docker run -d -p 7687:7687 -e NEO4J_AUTH=none neo4j:3.2.2
+export COTOAMI_NEO4J_HOST=$DOCKER_HOST_IP
+
 # Mail server
 echo
 echo "# Running maildev..."
