904d79c6b903089af0f2aa99a5bf397f16ce1db9
Daisuke Morita <daisuke.marubinotto@gmail.com>
2017-08-02 15:10:21 +0900

    Add App.Server.Graph.decodeCoto

--- elm/src/App/Server/Graph.elm
+++ elm/src/App/Server/Graph.elm
@@ -2,6 +2,9 @@ module App.Server.Graph exposing (..)
 
 import Json.Decode as Decode
 import App.Types.Graph exposing (Connection, initConnection)
+import App.Types.Coto exposing (Coto, initCoto)
+import App.Server.Amishi exposing (decodeAmishi)
+import App.Server.Cotonoma exposing (decodeCotonoma)
 
 
 decodeConnection : Decode.Decoder Connection
@@ -9,3 +12,13 @@ decodeConnection =
     Decode.map2 initConnection
         (Decode.maybe (Decode.field "start" Decode.string))
         (Decode.field "end" Decode.string)
+
+
+decodeCoto : Decode.Decoder Coto
+decodeCoto =
+    Decode.map5 initCoto
+        (Decode.field "uuid" Decode.string)
+        (Decode.field "content" Decode.string)
+        (Decode.maybe (Decode.field "amishi" decodeAmishi))
+        (Decode.maybe (Decode.field "posted_in" decodeCotonoma))
+        (Decode.maybe (Decode.field "cotonoma_key" Decode.string))
