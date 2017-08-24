3aec9a7afc6b082c9d97f4824723aa53811ee15b
Daisuke Morita <daisuke.marubinotto@gmail.com>
2017-08-24 20:39:19 +0900

    Add a Msg: PinCoto CotoId]

--- elm/src/App/Messages.elm
+++ elm/src/App/Messages.elm
@@ -53,6 +53,7 @@ type Msg
     | CotonomaClick CotonomaKey
     | DeleteCoto Coto
     | CotoDeleted (Result Http.Error String)
+    | PinCoto CotoId
     | ConfirmUnpinCoto CotoId
     | UnpinCoto CotoId
     | CotoUnpinned (Result Http.Error String)
