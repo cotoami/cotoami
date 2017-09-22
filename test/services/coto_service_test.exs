ade163d45605f21070db242d4ada4d21bd65b218
Daisuke Morita <daisuke.marubinotto@gmail.com>
2017-09-22 21:51:29 +0900

    Create Cotoami.CotoServiceTest

---
+++ test/services/coto_service_test.exs
@@ -0,0 +1,26 @@
+defmodule Cotoami.CotoServiceTest do
+  use Cotoami.ModelCase
+  import ShorterMaps
+  alias Cotoami.{
+    CotoService, AmishiService, CotoGraphService, Coto
+  }
+
+  setup do
+    amishi = AmishiService.create!("amishi@example.com")
+    %{conn: Bolt.Sips.conn, amishi: amishi}
+  end
+
+  describe "a coto pinned to an amishi" do
+    setup ~M{conn, amishi} do
+      {coto, _posted_in} = CotoService.create!(nil, amishi.id, "hello")
+      CotoGraphService.pin!(conn, coto, amishi)
+      ~M{coto}
+    end
+
+    test "get", ~M{conn, coto} do
+      assert %Coto{
+        content: "hello"
+      } = CotoService.get(coto.id)
+    end
+  end
+end
