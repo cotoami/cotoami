d492f47dbd197e897adf54f4869b85afdca75c73
Daisuke Morita <daisuke.marubinotto@gmail.com>
2017-10-05 15:17:49 +0900

    Add Cotoami.Email.invitation

--- lib/cotoami/email.ex
+++ lib/cotoami/email.ex
@@ -4,6 +4,7 @@ defmodule Cotoami.Email do
   """
 
   use Bamboo.Phoenix, view: Cotoami.EmailView
+  alias Cotoami.Amishi
 
   def signin_link(email_address, token, host_url) do
     new_email()
@@ -14,6 +15,20 @@ defmodule Cotoami.Email do
     |> render("signin_link.text",
       token: token,
       host_url: host_url)
+  end
+
+  def invitation(email_address, token, host_url,
+      %Amishi{email: inviter_email, display_name: inviter_name}) do
+    new_email()
+    |> to(email_address)
+    |> from({"Cotoami", from()})
+    |> subject("#{inviter_name} has invited you to join a Cotoami workspace")
+    |> put_text_layout({Cotoami.LayoutView, "email.text"})
+    |> render("invitation.text",
+      token: token,
+      host_url: host_url,
+      inviter_email: inviter_email,
+      inviter_name: inviter_name)
   end
 
   defp from do
