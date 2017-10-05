cf465b1dc8d20057242911540845e0bdf6c12788
Daisuke Morita <daisuke.marubinotto@gmail.com>
2017-10-05 11:42:41 +0900

    Introduce tool buttons in ProfileModal

--- elm/src/App/Modals/ProfileModal.elm
+++ elm/src/App/Modals/ProfileModal.elm
@@ -3,7 +3,7 @@ module App.Modals.ProfileModal exposing (view)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Util.Modal as Modal
-import Util.HtmlUtil exposing (faIcon)
+import Util.HtmlUtil exposing (faIcon, materialIcon)
 import App.Types.Session exposing (Session)
 import App.Messages exposing (Msg(CloseModal))
 
@@ -26,51 +26,58 @@ modalConfig session =
     { closeMessage = CloseModal
     , title = "Amishi Profile"
     , content =
-        div []
-            [ div [ class "profile container" ]
-                [ div [ class "row" ]
+        div [ class "profile container" ]
+            [ div [ class "row" ]
+                [ div
+                    [ class "profile-sidebar three columns" ]
                     [ div
-                        [ class "profile-sidebar three columns" ]
-                        [ div
-                            [ class "avatar-box" ]
-                            [ a [ href "https://gravatar.com/", target "_blank" ]
-                                [ img [ class "avatar", src session.avatarUrl ] [] ]
-                            ]
-                        , if session.owner then
-                            div
-                                [ class "owner-label" ]
-                                [ faIcon "key" (Just "owner-icon")
-                                , text "Owner"
-                                ]
-                          else
-                            div [] []
+                        [ class "avatar-box" ]
+                        [ a [ href "https://gravatar.com/", target "_blank" ]
+                            [ img [ class "avatar", src session.avatarUrl ] [] ]
                         ]
-                    , div
-                        [ class "profile-content nine columns" ]
-                        [ label [] [ text "Name" ]
-                        , input
-                            [ type_ "text"
-                            , class "u-full-width"
-                            , value session.displayName
-                            , disabled True
+                    , if session.owner then
+                        div
+                            [ class "owner-label" ]
+                            [ faIcon "key" (Just "owner-icon")
+                            , text "Owner"
                             ]
-                            []
-                        , label [] [ text "Email Address" ]
-                        , input
-                            [ type_ "text"
-                            , class "u-full-width"
-                            , value session.email
-                            , disabled True
-                            ]
-                            []
+                      else
+                        div [] []
+                    ]
+                , div
+                    [ class "profile-content nine columns" ]
+                    [ label [] [ text "Name" ]
+                    , input
+                        [ type_ "text"
+                        , class "u-full-width"
+                        , value session.displayName
+                        , disabled True
                         ]
+                        []
+                    , label [] [ text "Email Address" ]
+                    , input
+                        [ type_ "text"
+                        , class "u-full-width"
+                        , value session.email
+                        , disabled True
+                        ]
+                        []
+                    ]
+                ]
+            , div [ class "tools" ]
+                [ a
+                    [ class "tool-button traverse-coto"
+                    , title "Export my data"
+                    , href "/export"
+                    ]
+                    [ materialIcon "cloud_download" Nothing
+                    , br [] []
+                    , span [ class "label" ] [ text "Export" ]
                     ]
                 ]
             ]
     , buttons =
-        [ a [ class "button", href "/export" ]
-            [ text "Export my data" ]
-        , a [ class "button", href "/signout" ]
+        [ a [ class "button", href "/signout" ]
             [ text "Sign out" ]
         ]
     }
