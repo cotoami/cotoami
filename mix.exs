3a74968dd748c7717b92b263abcd86505b228c2a
Daisuke Morita <daisuke.marubinotto@gmail.com>
2017-10-17 15:00:26 +0900

    Bump version to 0.2.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Cotoami.Mixfile do
   def project do
     [
       app: :cotoami,
-      version: "0.2.1",
+      version: "0.2.2",
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
