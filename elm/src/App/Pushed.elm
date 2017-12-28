adeb252c7ec013d2784de6c790ffb0314f9d7b20
Daisuke Morita <daisuke.marubinotto@gmail.com>
2017-12-28 15:36:09 +0900

    Refactor the functions in App.Pushed

--- elm/src/App/Pushed.elm
+++ elm/src/App/Pushed.elm
@@ -10,32 +10,32 @@ import App.Channels exposing (Payload)
 
 
 type alias Handler body =
-    String -> Payload body -> Model -> ( Model, Cmd Msg )
+    Payload body -> Model -> ( Model, Cmd Msg )
 
 
 handle : String -> Decode.Decoder body -> Handler body -> Value -> Model -> ( Model, Cmd Msg )
 handle bodyName payloadDecoder handler payload model =
     case Decode.decodeValue (App.Channels.decodePayload bodyName payloadDecoder) payload of
         Ok decodedPayload ->
-            handler model.context.clientId decodedPayload model
+            if decodedPayload.clientId /= model.context.clientId then
+                handler decodedPayload model
+            else
+                ( model, Cmd.none )
 
         Err err ->
             ( model, Cmd.none )
 
 
-handlePost : String -> Payload Post -> Model -> ( Model, Cmd Msg )
-handlePost clientId payload model =
-    if payload.clientId /= clientId then
-        (model.timeline
-            |> (\timeline -> ( timeline, payload.body :: timeline.posts ))
-            |> (\( timeline, posts ) -> { timeline | posts = posts })
-            |> (\timeline -> { model | timeline = timeline })
-        )
-            ! if payload.body.asCotonoma then
-                [ App.Commands.scrollTimelineToBottom NoOp
-                , App.Commands.sendMsg (CotonomaPushed payload.body)
-                ]
-              else
-                [ App.Commands.scrollTimelineToBottom NoOp ]
-    else
-        ( model, Cmd.none )
+handlePost : Payload Post -> Model -> ( Model, Cmd Msg )
+handlePost payload model =
+    (model.timeline
+        |> (\timeline -> ( timeline, payload.body :: timeline.posts ))
+        |> (\( timeline, posts ) -> { timeline | posts = posts })
+        |> (\timeline -> { model | timeline = timeline })
+    )
+        ! if payload.body.asCotonoma then
+            [ App.Commands.scrollTimelineToBottom NoOp
+            , App.Commands.sendMsg (CotonomaPushed payload.body)
+            ]
+          else
+            [ App.Commands.scrollTimelineToBottom NoOp ]
