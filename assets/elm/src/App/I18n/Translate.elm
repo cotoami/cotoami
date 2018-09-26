e1fac4c11c40ee2ef959418b326c13225178a026
kroschu <kroschu@email.ua>
2018-09-26 10:37:51 +0300

    Update Translate.elm

--- assets/elm/src/App/I18n/Translate.elm
+++ assets/elm/src/App/I18n/Translate.elm
@@ -3,18 +3,21 @@ module App.I18n.Translate exposing (text)
 import App.I18n.Keys exposing (TextKey)
 import App.I18n.Translations.En
 import App.I18n.Translations.Ja
-import App.I18n.Translations.Ua
+import App.I18n.Translations.Uk
 
 text : String -> TextKey -> String
 text lang key =
     let
         translation =
             case lang of
-                "ua" ->
-                    App.I18n.Translations.Ua.text
-
                 "en" ->
                     App.I18n.Translations.En.text
+
+                "uk" ->
+                    App.I18n.Translations.Uk.text
+
+              "uk-UA" -> 
+                    App.I18n.Translations.Uk.text
 
                 "ja" ->
                     App.I18n.Translations.Ja.text
