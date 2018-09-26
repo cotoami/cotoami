800c92909af8a8743e082a4563b24cd4641c9b65
Daisuke Morita <daisuke.marubinotto@gmail.com>
2018-09-26 15:53:24 +0900

    Add a test to DatabaseControllerTest

--- test/controllers/database_controller_test.exs
+++ test/controllers/database_controller_test.exs
@@ -16,15 +16,17 @@ defmodule CotoamiWeb.DatabaseControllerTest do
     %{conn: conn}
   end
 
+  @amishi_id_in_import "1983b738-b9eb-4a20-88c1-b6e9c92fa84c"
+
   @amishi_in_import """
     {
-      "id": "7c382e93-c470-4922-b99d-9a179ea692f6",
+      "updated_at": 1537943474281,
+      "owner": true,
+      "inserted_at": 1537943474274,
+      "id": "#{@amishi_id_in_import}",
       "email": "daisuke.marubinotto@gmail.com",
       "display_name": "Daisuke Morita",
-      "avatar_url": "https://secure.gravatar.com/avatar/1d413392f15b8659a825fb6bab7396a9",
-      "owner": true,
-      "updated_at": 1537858884306,
-      "inserted_at": 1537858884302
+      "avatar_url": "https://secure.gravatar.com/avatar/1d413392f15b8659a825fb6bab7396a9"
     }
   """
 
@@ -32,19 +34,19 @@ defmodule CotoamiWeb.DatabaseControllerTest do
     test "import one coto", %{conn: conn} do
       test_import_and_export(
         conn,
-        fn (amishi_json) ->
+        fn (amishi_id, amishi_json) ->
           """
             {
               "cotos": [
                 {
-                  "id": "5f00e846-6076-4b13-a062-bbe15d2cbffe",
-                  "content": "a",
+                  "updated_at": 1537943597410,
                   "summary": null,
                   "posted_in_id": null,
-                  "as_cotonoma": false,
+                  "inserted_at": 1537943597410,
+                  "id": "e4910f38-5756-4ac4-a995-09d14cfa5874",
                   "cotonoma": null,
-                  "updated_at": 1536569026520,
-                  "inserted_at": 1536569026520
+                  "content": "hello",
+                  "as_cotonoma": false
                 }
               ],
               "connections": [],
@@ -54,17 +56,53 @@ defmodule CotoamiWeb.DatabaseControllerTest do
         end
       )
     end
+
+    test "import one coto pinned", %{conn: conn} do
+      test_import_and_export(
+        conn,
+        fn (amishi_id, amishi_json) ->
+          """
+            {
+              "cotos": [
+                {
+                  "updated_at": 1537943597410,
+                  "summary": null,
+                  "posted_in_id": null,
+                  "inserted_at": 1537943597410,
+                  "id": "e4910f38-5756-4ac4-a995-09d14cfa5874",
+                  "cotonoma": null,
+                  "content": "hello",
+                  "as_cotonoma": false
+                }
+              ],
+              "connections": [
+                {
+                  "start": "#{amishi_id}",
+                  "order": 1,
+                  "end": "e4910f38-5756-4ac4-a995-09d14cfa5874",
+                  "created_by": "#{amishi_id}",
+                  "created_at": 1537943695452
+                }
+              ],
+              "amishi": #{amishi_json}
+            }
+          """
+        end
+      )
+    end
   end
 
   defp test_import_and_export(conn, create_test_data) do
-    import_data = create_test_data.(@amishi_in_import)
+    import_data = create_test_data.(@amishi_id_in_import, @amishi_in_import)
     post(conn, "/api/import", %{"data" => import_data})
 
-    expected_export_data = 
+    amishi_json_in_export =
       conn.assigns.amishi
       |> Phoenix.View.render_one(AmishiView, "export.json")
       |> Poison.encode!()
-      |> create_test_data.()
+
+    expected_export_data = 
+      create_test_data.(conn.assigns.amishi.id, amishi_json_in_export)
       |> format_json()
 
     conn = get(conn, "/export")
