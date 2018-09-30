c06e0b415f7ef2d6fdbffac60bb7da4cc46724c1
Daisuke Morita <daisuke.marubinotto@gmail.com>
2018-09-30 19:44:04 +0900

    Add Chinese cases to App.I18n.Translate

--- assets/elm/src/App/I18n/Translate.elm
+++ assets/elm/src/App/I18n/Translate.elm
@@ -5,6 +5,8 @@ import App.I18n.Translations.En
 import App.I18n.Translations.Ja
 import App.I18n.Translations.Uk
 import App.I18n.Translations.Ru
+import App.I18n.Translations.ZhCn
+import App.I18n.Translations.ZhTw
 
 
 text : String -> TextKey -> String
@@ -26,6 +28,18 @@ text lang key =
 
                 "ru-RU" ->
                     App.I18n.Translations.Ru.text
+
+                "zh" ->
+                    App.I18n.Translations.ZhCn.text
+
+                "zh-CN" ->
+                    App.I18n.Translations.ZhCn.text
+
+                "zh-TW" ->
+                    App.I18n.Translations.ZhTw.text
+
+                "zh-HK" ->
+                    App.I18n.Translations.ZhTw.text
 
                 "ja" ->
                     App.I18n.Translations.Ja.text
