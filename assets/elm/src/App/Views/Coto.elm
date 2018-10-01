e6213e31a41b4db0b36c14eac820bbb4a861e348
Daisuke Morita <daisuke.marubinotto@gmail.com>
2018-10-01 18:20:25 +0900

    Add App.Views.Reorder.reorderTools

--- assets/elm/src/App/Views/Coto.elm
+++ assets/elm/src/App/Views/Coto.elm
@@ -26,6 +26,7 @@ import App.Types.Graph exposing (Direction(..), Graph, Connection, InboundConnec
 import App.Submodels.Context exposing (Context)
 import App.Messages exposing (..)
 import App.Views.CotoToolbar
+import App.Views.Reorder
 
 
 cotoClassList : Context a -> ElementId -> Maybe CotoId -> List ( String, Bool ) -> Attribute msg
@@ -137,7 +138,7 @@ headerDiv context graph maybeInbound elementId coto =
         [ if App.Submodels.Context.inReorderMode elementId context then
             maybeInbound
                 |> Maybe.map
-                    (\inbound -> reorderToolButtonsSpan context inbound elementId)
+                    (\inbound -> App.Views.Reorder.reorderTools context inbound elementId)
                 |> Maybe.withDefault
                     Utils.HtmlUtil.none
           else
@@ -173,71 +174,6 @@ headerDiv context graph maybeInbound elementId coto =
           else
             Utils.HtmlUtil.none
         ]
-
-
-reorderToolButtonsSpan : Context a -> InboundConnection -> ElementId -> Html Msg
-reorderToolButtonsSpan context inbound elementId =
-    let
-        maybeParentId =
-            Maybe.map (.id) inbound.parent
-
-        index =
-            inbound.index
-
-        isFirst =
-            inbound.index == 0
-
-        isLast =
-            inbound.index == (inbound.siblings - 1)
-    in
-        span [ class "reorder-tool-buttons" ]
-            [ a
-                [ classList
-                    [ ( "tool-button", True )
-                    , ( "move-to-top", True )
-                    , ( "disabled", isFirst )
-                    ]
-                , title "Move to the top"
-                , onLinkButtonClick (MoveToFirst maybeParentId index)
-                ]
-                [ materialIcon "skip_previous" Nothing ]
-            , a
-                [ classList
-                    [ ( "tool-button", True )
-                    , ( "move-up", True )
-                    , ( "disabled", isFirst )
-                    ]
-                , title "Move up"
-                , onLinkButtonClick (SwapOrder maybeParentId index (index - 1))
-                ]
-                [ materialIcon "play_arrow" Nothing ]
-            , a
-                [ classList
-                    [ ( "tool-button", True )
-                    , ( "move-down", True )
-                    , ( "disabled", isLast )
-                    ]
-                , title "Move down"
-                , onLinkButtonClick (SwapOrder maybeParentId index (index + 1))
-                ]
-                [ materialIcon "play_arrow" Nothing ]
-            , a
-                [ classList
-                    [ ( "tool-button", True )
-                    , ( "move-to-bottom", True )
-                    , ( "disabled", isLast )
-                    ]
-                , title "Move to the bottom"
-                , onLinkButtonClick (MoveToLast maybeParentId index)
-                ]
-                [ materialIcon "skip_next" Nothing ]
-            , a
-                [ class "tool-button close"
-                , title "Close reorder tools"
-                , onLinkButtonClick (ToggleReorderMode elementId)
-                ]
-                [ materialIcon "close" Nothing ]
-            ]
 
 
 
--- assets/elm/src/App/Views/Reorder.elm
+++ assets/elm/src/App/Views/Reorder.elm
@@ -1,11 +1,84 @@
 module App.Views.Reorder
     exposing
         ( Reordering(..)
+        , reorderTools
         )
 
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Utils.EventUtil exposing (onLinkButtonClick)
+import Utils.HtmlUtil exposing (materialIcon)
+import App.Messages exposing (..)
 import App.Types.Coto exposing (ElementId)
+import App.Submodels.Context exposing (Context)
+import App.Types.Graph exposing (InboundConnection)
 
 
 type Reordering
     = PinnedCotos
     | SubCotos ElementId
+
+
+reorderTools : Context a -> InboundConnection -> ElementId -> Html Msg
+reorderTools context inbound elementId =
+    let
+        maybeParentId =
+            Maybe.map (.id) inbound.parent
+
+        index =
+            inbound.index
+
+        isFirst =
+            inbound.index == 0
+
+        isLast =
+            inbound.index == (inbound.siblings - 1)
+    in
+        span [ class "reorder-tool-buttons" ]
+            [ a
+                [ classList
+                    [ ( "tool-button", True )
+                    , ( "move-to-top", True )
+                    , ( "disabled", isFirst )
+                    ]
+                , title "Move to the top"
+                , onLinkButtonClick (MoveToFirst maybeParentId index)
+                ]
+                [ materialIcon "skip_previous" Nothing ]
+            , a
+                [ classList
+                    [ ( "tool-button", True )
+                    , ( "move-up", True )
+                    , ( "disabled", isFirst )
+                    ]
+                , title "Move up"
+                , onLinkButtonClick (SwapOrder maybeParentId index (index - 1))
+                ]
+                [ materialIcon "play_arrow" Nothing ]
+            , a
+                [ classList
+                    [ ( "tool-button", True )
+                    , ( "move-down", True )
+                    , ( "disabled", isLast )
+                    ]
+                , title "Move down"
+                , onLinkButtonClick (SwapOrder maybeParentId index (index + 1))
+                ]
+                [ materialIcon "play_arrow" Nothing ]
+            , a
+                [ classList
+                    [ ( "tool-button", True )
+                    , ( "move-to-bottom", True )
+                    , ( "disabled", isLast )
+                    ]
+                , title "Move to the bottom"
+                , onLinkButtonClick (MoveToLast maybeParentId index)
+                ]
+                [ materialIcon "skip_next" Nothing ]
+            , a
+                [ class "tool-button close"
+                , title "Close reorder tools"
+                , onLinkButtonClick (ToggleReorderMode elementId)
+                ]
+                [ materialIcon "close" Nothing ]
+            ]
