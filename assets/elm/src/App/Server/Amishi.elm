6f51cd5ba7d270a9f63d3defd9e889a048b91300
Daisuke Morita <daisuke.marubinotto@gmail.com>
2018-10-02 15:54:09 +0900

    Add inviteLimit to App.Types.Amishi

--- assets/elm/src/App/Server/Amishi.elm
+++ assets/elm/src/App/Server/Amishi.elm
@@ -1,19 +1,20 @@
 module App.Server.Amishi exposing (decodeAmishi, fetchAmishi)
 
 import Http
-import Json.Decode as Decode exposing (maybe, string, bool)
+import Json.Decode as Decode exposing (maybe, string, bool, int)
 import App.Types.Amishi exposing (Amishi)
 
 
 decodeAmishi : Decode.Decoder Amishi
 decodeAmishi =
-    Decode.map6 Amishi
+    Decode.map7 Amishi
         (Decode.field "id" string)
         (Decode.field "email" (maybe string))
         (Decode.field "auth_provider" (maybe string))
         (Decode.field "owner" bool)
         (Decode.field "avatar_url" string)
         (Decode.field "display_name" string)
+        (Decode.field "invite_limit" (maybe int))
 
 
 fetchAmishi : (Result Http.Error Amishi -> msg) -> String -> Cmd msg
