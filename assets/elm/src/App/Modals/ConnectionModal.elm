b06f14bd5f543aec6ae748c497a56b64f709ed31
Daisuke Morita <daisuke.marubinotto@gmail.com>
2019-02-22 13:23:43 +0900

    Add App.Modals.ConnectionModal.modalContent

--- assets/elm/src/App/Modals/ConnectionModal.elm
+++ assets/elm/src/App/Modals/ConnectionModal.elm
@@ -8,6 +8,7 @@ import App.I18n.Keys as I18nKeys
 import App.Messages as AppMsg exposing (Msg(CloseModal))
 import App.Submodels.Context exposing (Context)
 import App.Types.Coto exposing (Coto)
+import App.Views.Connection
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
@@ -41,7 +42,7 @@ modalConfig : Context context -> Model -> Utils.Modal.Config AppMsg.Msg
 modalConfig context model =
     { closeMessage = CloseModal
     , title = text (context.i18nText I18nKeys.ConnectionModal_Title)
-    , content = div [] []
+    , content = modalContent context model
     , buttons =
         [ button
             [ class "button disconnect"
@@ -61,3 +62,22 @@ modalConfig context model =
             [ text (context.i18nText I18nKeys.Save) ]
         ]
     }
+
+
+modalContent : Context context -> Model -> Html AppMsg.Msg
+modalContent context model =
+    div [ id "connection" ]
+        [ div
+            [ class "start" ]
+            [ span [ class "node-title" ] [ text "From:" ]
+            , App.Views.Connection.cotoDiv model.startCoto
+            ]
+        , App.Views.Connection.linkingPhraseInputDiv
+            context
+            (\_ -> AppMsg.NoOp)
+        , div
+            [ class "end" ]
+            [ span [ class "node-title" ] [ text "To:" ]
+            , App.Views.Connection.cotoDiv model.endCoto
+            ]
+        ]
