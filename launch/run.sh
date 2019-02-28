86979e31a9ce52e9cd899a39684ee0e7b3c78b11
Daisuke Morita <daisuke.marubinotto@gmail.com>
2019-02-28 22:40:48 +0900

    Update README.md

--- launch/run.sh
+++ launch/run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-wget -q https://raw.githubusercontent.com/cotoami/cotoami/develop/launch/docker-compose.yml -O docker-compose.yml
-
 if [ -n "$DOCKER_HOST" ]; then
   DOCKER_HOST_IP=$(echo $DOCKER_HOST | sed 's/^.*\/\/\(.*\):[0-9][0-9]*$/\1/g')
 else
@@ -12,7 +10,10 @@ export COMPOSE_PROJECT_NAME=cotoami
 export COTOAMI_VERSION=develop
 export COTOAMI_HOST=$DOCKER_HOST_IP
 
+wget -q https://raw.githubusercontent.com/cotoami/cotoami/$COTOAMI_VERSION/launch/docker-compose.yml -O docker-compose.yml
+
 docker-compose up -d
 
+echo
 echo "Cotoami will be ready at http://$DOCKER_HOST_IP:4000"
 echo "You can check sign-up/in mails at http://$DOCKER_HOST_IP:8080"
