ddd506d25e15c75396ef30020a07715a9f5bb812
Daisuke Morita <daisuke.marubinotto@gmail.com>
2019-05-06 15:15:25 +0900

    Add App.Submodels.CotoSelection

---
+++ assets/elm/src/App/Submodels/CotoSelection.elm
@@ -0,0 +1,65 @@
+module App.Submodels.CotoSelection exposing
+    ( CotoSelection
+    , anySelection
+    , clearSelection
+    , deselect
+    , finishBeingDeselected
+    , isSelected
+    , setBeingDeselected
+    , toggleSelection
+    )
+
+import App.Types.Coto exposing (Coto, CotoId)
+import Set exposing (Set)
+
+
+type alias CotoSelection a =
+    { a
+        | selection : List Coto
+        , deselecting : Set CotoId
+    }
+
+
+anySelection : CotoSelection model -> Bool
+anySelection model =
+    not (List.isEmpty model.selection)
+
+
+isSelected : CotoId -> CotoSelection model -> Bool
+isSelected cotoId model =
+    List.any (\coto -> coto.id == cotoId) model.selection
+
+
+deselect : CotoId -> CotoSelection model -> CotoSelection model
+deselect cotoId model =
+    { model | selection = List.filter (\coto -> coto.id /= cotoId) model.selection }
+
+
+toggleSelection : Coto -> CotoSelection model -> CotoSelection model
+toggleSelection coto model =
+    if isSelected coto.id model then
+        deselect coto.id model
+
+    else
+        { model | selection = coto :: model.selection }
+
+
+clearSelection : CotoSelection model -> CotoSelection model
+clearSelection model =
+    { model | selection = [] }
+
+
+setBeingDeselected : CotoId -> CotoSelection model -> CotoSelection model
+setBeingDeselected cotoId model =
+    { model | deselecting = Set.insert cotoId model.deselecting }
+
+
+finishBeingDeselected : CotoSelection model -> CotoSelection model
+finishBeingDeselected model =
+    { model
+        | selection =
+            List.filter
+                (\coto -> not (Set.member coto.id model.deselecting))
+                model.selection
+        , deselecting = Set.empty
+    }
